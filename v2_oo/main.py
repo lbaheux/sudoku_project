b62d1e3b6dd8c522a1900f023a98e8af447f2114
lbaheux <louis.baheux1@gmail.com>
2025-06-28 06:29:39 +0200

    suppression de code obsolète

--- v2_oo/main.py
+++ v2_oo/main.py
@@ -7,17 +7,3 @@ board = generator.generate_full_board()
 
 print("grille générée:")
 board.display()
-
-
-"""
-board = SudokuBoard(grid)
-print("Grille initiale :")
-board.display()
-
-if board.solve():
-    print("\nGrille résolue :")
-    board.display()
-else:
-    print("Pas de solution trouvée.")
-"""
-
